8bcc0bb0297405250ca37520b3f53d1152684286
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-05-11 15:13:56 +0500

    on working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 
 export default function Home() {
   return (
-   <div>
-Hello World
+   <div className="main">
+<div className="overlap">
+<img src="/images/shawal.jpg"/>
+<h1>Shawaal Nadeem</h1>
+</div>
+<div className="about-section">
+<h2>ABOUT ME</h2>
+<p>My technical skills include proficiency in programming languages such as Typescript, Javascript, Python and C++, as well as experience with database management and web development. In addition, I have developed strong communication and leadership skills through various group projects and extracurricular activities.</p>
+<br/>
+<p>When I'm not working, I enjoy hiking, reading, and spending time with my friends and family. I believe that my determination, can-do attitude, and hard work make me a true asset to any team, and I am excited to bring these qualities to a new opportunity.</p>
+</div>
+
    </div>
   )
 }
