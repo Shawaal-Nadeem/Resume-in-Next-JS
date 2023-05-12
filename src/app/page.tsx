edf8898734347706c0c33946d773896a11802a20
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-05-12 16:58:41 +0500

    working on social icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-
+import { FaTwitter } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import{AiFillGithub} from 'react-icons/ai'
 export default function Home() {
   return (
    <div className="main">
 <div className="overlap">
 <img src="/images/shawal.jpg"/>
 <h1>Shawaal Nadeem</h1>
+<h3> Software Engineer</h3>
+<br/>
+<p>Bachelor of Science in Computer Science, University of Central Punjab(2021-2025)</p>
+<p>Web 3.O & Metaverse, PIAIC (2023-2024)</p>
+<br/>
+<h5>CONTACT ME</h5>
+<div className='icons'>
+<a href='mailto:hafizshawalnadeem@gmail.com'><FaEnvelope style={{color:'white',cursor:'pointer'}} size={25} /></a>
+<FaTwitter style={{color:'white',cursor:'pointer',marginLeft:'20px'}} size={25} />
+<FaLinkedin style={{color:'white',cursor:'pointer',marginLeft:'20px'}} size={25} />
+<AiFillGithub style={{color:'white',cursor:'pointer',marginLeft:'20px'}} size={25} />
+</div>
+
 </div>
 <div className="about-section">
 <h2>ABOUT ME</h2>
