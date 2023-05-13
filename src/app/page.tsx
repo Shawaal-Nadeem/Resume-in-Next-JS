d0d09d6411f05bc38d280f45892e67a1b4beb2ae
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-05-13 17:14:41 +0500

    complete (no responsive)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
 <h2>ABOUT ME</h2>
 <p>My technical skills include proficiency in programming languages such as Typescript, Javascript, Python and C++, as well as experience with database management and web development. In addition, I have developed strong communication and leadership skills through various group projects and extracurricular activities.</p>
 <br/>
-<p>When I'm not working, I enjoy hiking, reading, and spending time with my friends and family. I believe that my determination, can-do attitude, and hard work make me a true asset to any team, and I am excited to bring these qualities to a new opportunity.</p>
+<p>When I am not working, I enjoy hiking, reading, and spending time with my friends and family. I believe that my determination, can-do attitude, and hard work make me a true asset to any team, and I am excited to bring these qualities to a new opportunity.</p>
 <div className='buttons'>
 <button className='b1'>SOFT SKILLS</button>
 <button className='b2'>HARD SKILLS</button>
